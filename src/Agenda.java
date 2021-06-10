24dfadeac5f4104c8367853f031eed8c7e46ba39
Carlos Munoz <carlos.munoz.us03@gmail.com>
2021-06-10 15:27:37 -0500

    first part exercises

---
+++ src/Agenda.java
@@ -0,0 +1,98 @@
+public class Agenda {
+    private Contact[] contacts;
+
+    public Agenda(Contact[] contacts) {
+        this.contacts = new Contact[10];
+    }
+
+    public Agenda(int size) {
+        this.contacts = new Contact[size];
+    }
+
+    public void addContact(Contact c) {
+        if (existContact(c)) {
+            System.out.println("This contact already exists");
+        } else if (fullAgenda()) {
+            System.out.println("Agenda is full, you cannot add more contacts");
+        } else {
+
+            boolean found = false;
+            for (int i = 0; i < contacts.length && !found; i++) {
+                if (contacts[i] == null) {
+                    contacts[i] = c;
+                    found = true;
+                }
+            }
+            if (found) {
+                System.out.println("Your add a new contact!");
+            } else {
+                System.out.println("We could not add a new contact :( ");
+            }
+        }
+
+    }
+
+    public boolean existContact(Contact c) {
+
+        for (int i = 0; i < contacts.length; i++) {
+            if (contacts[i] == null && c.equals(contacts[i])) {
+                return true;
+
+            }
+        }
+        return false;
+    }
+
+    public void contactList() {
+        for (int i = 0; i < contacts.length; i++) {
+            if (contacts[i] == null) {
+                System.out.println(contacts[i]);
+            }
+        }
+    }
+
+    public void searchName(String name) {
+        boolean found = false;
+        for (int i = 0; i < contacts.length && !found; i++) {
+            if (contacts[i] == null && contacts[i].getName().equalsIgnoreCase(name)) {
+                System.out.println("Your number is: " + contacts[i].getNumber());
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("contact not found");
+        }
+    }
+
+    public boolean fullAgenda() {
+        for (int i = 0; i < contacts.length; i++) {
+            if (contacts[i] == null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void removeContact(Contact c) {
+        boolean found = false;
+        for (int i = 0; i < contacts.length && found; i++) {
+            if (contacts[i] == null && contacts[i].equals(c)) {
+                contacts = null;
+                found = true;
+            }
+        }
+        if (!found) {
+            System.out.println("contact has not been deleted");
+        }
+    }
+
+    public int freeSpots() {
+        int freeCount = 0;
+        for (int i = 0; i < contacts.length; i++) {
+            if (contacts[i] == null) {
+                freeCount++;
+            }
+        }
+        return freeCount;
+    }
+}
---
+++ src/Contact.java
@@ -0,0 +1,86 @@
+public class Contact {
+    private String name;
+    private String number;
+
+    public Contact(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public boolean equals(Contact c) {
+        if (this.name.equalsIgnoreCase(c.getName())) {
+            return true;
+        }
+        return false;
+    }
+
+
+    @Override
+
+    public String toString() {
+        return "Name: " + name + ", Number: " + number;
+    }
+}
+//    private String name;
+//    private String number;
+//    private ArrayList<String> contacts;
+//
+//    public Contacts(String name, String number) {
+//        this.name = name;
+//        this.number = number;
+//        this.contacts = new ArrayList<>();
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getNumber() {
+//        return number;
+//    }
+//
+//    public void setNumber(String number) {
+//        this.number = number;
+//    }
+//
+//    public ArrayList<String> getContacts() {
+//        return contacts;
+//    }
+//
+//    public void setContacts(ArrayList<String> contacts) {
+//        this.contacts = contacts;
+//    }
+//
+//    public void addContact(String newContacts) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Please enter new contact: ");
+//        String newContact = scanner.nextLine();
+//        contacts.add(newContact);
+//        System.out.println(contacts);
+//    }
+//
+//
+//    public static void main(String[] args) {
+//
+//    }
+//}
