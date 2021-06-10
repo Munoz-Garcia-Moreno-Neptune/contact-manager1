d58cf88f5e8cd5f44a8043aaf7e7a805f1b173fe
Savanna Marie <savannamarie@gmail.com>
2021-06-10 13:56:15 -0500

    starting project

---
+++ src/ContactTest.java
@@ -0,0 +1,5 @@
+public class ContactTest {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
--- src/Contacts.java
+++ src/Contacts.java
@@ -1,2 +1,41 @@
+import java.util.Scanner;
+
 public class Contacts {
+    private String name;
+    private String number;
+
+    public Contacts(String name, String number){
+        this.name = name;
+        this.number = number;
+    }
+
+    public void addContact(String name, String number){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter new contact: ");
+        String newContact = scanner.nextLine();
+        System.out.println(newContact);
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
 }
